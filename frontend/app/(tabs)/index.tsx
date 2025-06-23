1306534a3dde22788efff44e15ef3e1ef0934cce
kutah1 <jameskutah47@gmail.com>
2025-06-23 05:58:12 +0100

    Work on the frontend project here

---
+++ frontend/app/(tabs)/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native'
+import React from 'react'
+
+const index = () => {
+  return (
+    <View className='bg-white  flex-1 items-center justify-center'>
+      <Text className='text-4xl'>index</Text>
+    </View>
+  )
+}
+
+export default index
